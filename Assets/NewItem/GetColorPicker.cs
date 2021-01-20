021f688a517c6cc398f9bf20afd7b7dadc92ae29
Luna <malalunapg@gmail.com>
2021-01-20 17:42:19 -0500

    feat(item): Add item Creation

---
+++ Assets/NewItem/GetColorPicker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetColorPicker : MonoBehaviour
+{
+  public GameObject colorpicker;
+  public Button btn;
+    void Start()
+    {
+      if ( btn == null )
+      {
+        btn=GetComponent<Button> ( );
+        btn.onClick.AddListener (DisplayColorPicker);
+    }
+  }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  void DisplayColorPicker() {
+    colorpicker.SetActive (true);
+  }
+
+}
---
+++ Assets/NewItem/SetColor.cs
@@ -0,0 +1,24 @@
+﻿using HSVPicker;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetColor : MonoBehaviour
+{
+  public GameObject colorpicker;
+  public GameObject colorpickerPannel;
+  public GameObject target;
+  void Start()
+  {
+      Button btn=GetComponent<Button> ( );
+      btn.onClick.AddListener (getColor);
+  }
+  void getColor()
+  {
+    Color  color = colorpicker.GetComponent<ColorPicker> ().CurrentColor ;
+    Image targetColor = target.GetComponent<Image> ( );
+    targetColor.color=color;
+    colorpickerPannel.SetActive (false);
+  }
+}
