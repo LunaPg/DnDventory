9e82224dfb476687e1c02250e949d21bf4cba797
Luna <malalunapg@gmail.com>
2021-01-20 17:42:02 -0500

    feat(item): Add componenent

---
+++ Assets/Items/items.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class items 
+{
+
+  [SerializeField]
+  public int quantity = 0;
+
+  [SerializeField]
+  public string name = "";
+
+  [SerializeField]
+  public Sprite image;
+}
