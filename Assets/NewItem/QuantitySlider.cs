cf4363f0b9ea6a21c0b2608630a4379543c1a9a9
Luna <malalunapg@gmail.com>
2021-01-24 18:55:19 -0500

    feat(slider): Sync value between slider an quantity imput

---
+++ Assets/NewItem/QuantitySlider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuantitySlider : MonoBehaviour
+{
+  InputField quantity;
+  Slider slider;
+  // Start is called before the first frame update
+  void Start()
+  {
+    slider=this.GetComponentInChildren<Slider> ( );
+    quantity=this.GetComponentInChildren<InputField> ( );
+    slider.onValueChanged.AddListener (delegate { updateQuantity ( ); });
+    quantity.onValueChanged.AddListener(delegate { updateSlider ( ); });
+
+    if ( quantity.text!="" )
+    {
+      slider.value =Int32.Parse (quantity.text);
+    }
+  }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void updateQuantity()
+    {
+      quantity.text=slider.value.ToString();
+    }
+
+  void updateSlider()
+  {
+    slider.value=Int32.Parse (quantity.text);
+  }
+}
+
